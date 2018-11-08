12bbbcfdd83861f445cf4a47073b75d58916b343
Neng Vang <neng.vang@digitalglobe.com>
2018-11-08 21:38:53 +0000

    updating upload-release

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -6,11 +6,20 @@ set -x
 #But this does not demonostrate a best practice
 
 export CA_CERT_URL=https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-export BOSH_CLIENT_SECRET=<replace-me>
+#export BOSH_CLIENT_SECRET=<replace-me>
+export BOSH_CLIENT_SECRET=$(bosh int ~/creds.yml --path /admin_password)
 export BOSH_DEPLOYMENT=${GITHUB_USERNAME}-nginx
 export BOSH_DIRECTOR='https://10.4.1.4:25555'
 export BOSH_ENVIRONMENT='training'
-export BOSH_CLIENT=admin
+#export BOSH_CLIENT=admin
+export BOSH_CLIENT=$(bosh int ~/creds.yml --path /username)
+
+#export CA_CERT_URL=<S3_object_URL>
+#export BOSH_CLIENT_SECRET=<bosh password>
+#export BOSH_DEPLOYMENT=<deployment name>
+#export BOSH_DIRECTOR=<bosh director url>
+#export BOSH_ENVIRONMENT=<bosh director ip>
+#export BOSH_CLIENT=<bosh user>
 
 cd source-code/nginx_release
 
